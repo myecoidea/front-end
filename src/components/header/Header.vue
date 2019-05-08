3e2760e6d854723b84346bae61916f63119c0631
Quentin Sar <sarquentin@gmail.com>
2019-05-08 15:42:26 +0200

    0.0.1

---
+++ src/components/header/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-toolbar>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>My EcoIdea</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+            <v-btn>Acceuil</v-btn>
+            <v-btn>Partager une idée</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
